35140300b287dd22e4b35a1e2cc2b5132c6459df
Mateo <mateo.angulo1804@gmail.com>
2025-05-19 23:20:32 -0500

    dtos de sede, espacio y problema a record

--- src/main/java/edu/unimagdalena/reservasespacios/dtos/requests/ProblemaDtoRequest.java
+++ src/main/java/edu/unimagdalena/reservasespacios/dtos/requests/ProblemaDtoRequest.java
@@ -1,19 +1,10 @@
 package edu.unimagdalena.reservasespacios.dtos.requests;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.sql.Date;
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class ProblemaDtoRequest {
 
-    private Long espacioId;
-    private String estado;
-    private String descripcion;
-    private Date fecha;
-}
+public record ProblemaDtoRequest (
+        Long espacioId,
+        String estado,
+        String descripcion,
+        Date fecha
+){}
--- src/main/java/edu/unimagdalena/reservasespacios/dtos/response/ProblemaDtoResponse.java
+++ src/main/java/edu/unimagdalena/reservasespacios/dtos/response/ProblemaDtoResponse.java
@@ -2,10 +2,10 @@ package edu.unimagdalena.reservasespacios.dtos.response;
 
 import java.sql.Date;
 
-public class ProblemaDtoResponse {
-    private Long idProblema;
-    private Long espacioId;
-    private String estado;
-    private String descripcion;
-    private Date fecha;
-}
+public record ProblemaDtoResponse (
+        Long idProblema,
+        Long espacioId,
+        String estado,
+        String descripcion,
+        Date fecha
+){}
