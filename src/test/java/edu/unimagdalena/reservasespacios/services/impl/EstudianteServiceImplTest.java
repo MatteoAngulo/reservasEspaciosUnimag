0252445282b6d51fc605b61affcc66b777f05eeb
leonelp <pereiraleonel287@gmail.com>
2025-05-22 21:36:42 -0500

    antes de comenzar pruebas

---
+++ src/test/java/edu/unimagdalena/reservasespacios/services/impl/EstudianteServiceImplTest.java
@@ -0,0 +1,36 @@
+package edu.unimagdalena.reservasespacios.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EstudianteServiceImplTest {
+
+    @Test
+    void saveEstudiante() {
+    }
+
+    @Test
+    void findEstudianteById() {
+    }
+
+    @Test
+    void findEstudianteByCorreo() {
+    }
+
+    @Test
+    void findEstudianteByCodigoEstudiantil() {
+    }
+
+    @Test
+    void findEstudiantes() {
+    }
+
+    @Test
+    void updateEstudiante() {
+    }
+
+    @Test
+    void deleteEstudiante() {
+    }
+}
