1e538fbbc4554ffca50f3aa78cda891b9c44e8f3
leonelp <pereiraleonel287@gmail.com>
2025-05-22 23:31:24 -0500

    corrigiendo problema en EstudianteController

--- src/main/java/edu/unimagdalena/reservasespacios/controllers/EstudianteController.java
+++ src/main/java/edu/unimagdalena/reservasespacios/controllers/EstudianteController.java
@@ -37,19 +37,12 @@ public class EstudianteController {
         return ResponseEntity.ok(estudianteService.findEstudianteById(id));
     }
 
-    @GetMapping("/por-correo")
-    public ResponseEntity<EstudianteDTOResponse> findEstudianteByCorreo(@RequestParam
-                                                                        @NotBlank(message = "El correo no puede esta vacio")
-                                                                        String correo) {
-        return ResponseEntity.ok(estudianteService.findEstudianteByCorreo(correo));
-    }
-
     @GetMapping("/por-codigo")
     public ResponseEntity<EstudianteDTOResponse> findEstudianteByCodigo(@RequestParam
                                                                         @NotNull(message = "El codigo no puede estar vacio")
                                                                         @Positive(message = "El codigo no puede ser negativo")
-                                                                        String correo) {
-        return ResponseEntity.ok(estudianteService.findEstudianteByCorreo(correo));
+                                                                        Long codigo) {
+        return ResponseEntity.ok(estudianteService.findEstudianteByCodigoEstudiantil(codigo));
     }
 
     @GetMapping("/todos")
