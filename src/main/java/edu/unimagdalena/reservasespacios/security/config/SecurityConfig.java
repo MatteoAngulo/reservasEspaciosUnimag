4a4010caa9ac5a6a71277db3af6913d244480c3d
leonelp <pereiraleonel287@gmail.com>
2025-05-29 23:08:34 -0500

    security config security filter chain

--- src/main/java/edu/unimagdalena/reservasespacios/security/config/SecurityConfig.java
+++ src/main/java/edu/unimagdalena/reservasespacios/security/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception ->
